b05391f86a5b0a285d89c5bdb6f7b76eef8127a5
Rixud <Rixud@users.noreply.github.com>
2017-07-29 14:19:59 -0400

    player moviendose

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+    public float speed = 5;
+    public float playerAcceleration = 10;
+    private Rigidbody playerRigidBody;
+
+    public bool running;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+       
+        playerRigidBody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        if (playerRigidBody.velocity.z > speed)
+        {
+             
+        }
+        if (playerRigidBody.velocity.z < speed)
+        {
+            playerRigidBody.AddForce(transform.forward * playerAcceleration);
+        }
+		
+	}
+
+    void Update()
+    {
+        //Debug.Log(playerRigidBody.velocity);
+
+    }
+}
