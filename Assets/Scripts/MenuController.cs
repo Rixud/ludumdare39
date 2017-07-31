d5458f5f20fb4a322e29a15c0345ce183397e7b2
Carlos <carlosdegois@gmail.com>
2017-07-31 19:28:14 -0400

    Mnues

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+
+    private Button but;
+
+    // Use this for initialization
+    void Start () {
+        but = transform.Find("StartButton").GetComponent<Button>();
+        but.onClick.AddListener(IniciateGame);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void IniciateGame()
+    {
+        SceneManager.LoadScene("testCdg");
+    }
+}
